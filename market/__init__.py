75cfb2460aff727ce34ecc965b81bb820fa3b85e
Beatriz Galindo <beagalin@amazon.com>
2023-08-24 09:48:18 +0100

    Creating new files and adding some structure

--- market/__init__.py
+++ market/__init__.py
@@ -1,10 +1,8 @@
 from flask import Flask, render_template
-
 from flask_sqlalchemy import SQLAlchemy
-from datetime import datetime
-
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///market.db'
 # Initialise the database
 db = SQLAlchemy(app)
 
+from market import routes
