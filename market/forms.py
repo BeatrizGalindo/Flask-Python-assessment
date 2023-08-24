03dec4a53d89cfca995492e7f3936c589c10b130
Beatriz Galindo <beagalin@amazon.com>
2023-08-24 17:56:40 +0100

    Displaying error for users and emails already in the db

--- market/forms.py
+++ market/forms.py
@@ -1,8 +1,21 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
-from wtforms.validators import DataRequired, Length, EqualTo, Email
-
+from wtforms.validators import DataRequired, Length, EqualTo, Email, ValidationError
+from market.models import User
 class RegisterForm(FlaskForm):
+
+    # Validating that we don't repeat users in the database
+    def validate_username(self, username_to_check):
+        user = User.query.filter_by(username=username_to_check.data).first()
+        if user:
+            raise ValidationError('Username already exist')
+
+    def validate_email_address(self, email_address_to_check):
+        email_address = User.query.filter_by(email_address=email_address_to_check.data).first()
+        if email_address:
+            raise ValidationError('Email already exist')
+
+
     username = StringField(label='Write here your user name:', validators=[DataRequired(),Length(min=3, max=10)])
     email_address = StringField(label='Write here your email:', validators=[DataRequired(),Email()])
     password_hash = PasswordField(label='Password:', validators=[DataRequired(),Length(min=5)])
--- market/routes.py
+++ market/routes.py
@@ -1,5 +1,5 @@
 from market import app
-from flask import render_template, redirect, url_for
+from flask import render_template, redirect, url_for, flash
 from market.models import Item, User
 from market.forms import RegisterForm
 from market import db
@@ -35,6 +35,6 @@ def register_page():
     # Check that we don't have any errors in the validation for registers
     if form.errors != {}:
         for err_message in form.errors.values():
-            print(f'Error creating a user: {err_message}')
+            flash(f'Error creating a user: {err_message}', category='problem')
 
     return render_template(('register.html'), form = form)
