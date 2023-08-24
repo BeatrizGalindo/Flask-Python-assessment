41aab4882cea0e57e45c5f2b5925b265cbc84681
Beatriz Galindo <beagalin@amazon.com>
2023-08-24 15:43:57 +0100

    Adding the layout for the input of the register

--- market/forms.py
+++ market/forms.py
@@ -2,8 +2,8 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
 
 class RegisterForm(FlaskForm):
-    username = StringField(label='username')
-    email_address = StringField(label='email_address')
-    password_hash = PasswordField(label='password_hash')
-    password_hash_confirm = PasswordField(label='password_hash_confirm')
+    username = StringField(label='Write here your user name:')
+    email_address = StringField(label='Write here your email:')
+    password_hash = PasswordField(label='Password:')
+    password_hash_confirm = PasswordField(label='Confirm your password:')
     submit = SubmitField(label='submit')
