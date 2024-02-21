e7283a93e72f81165f314f66d8aa95d7fff4dcf2
Beatriz Galindo <beagalin@amazon.com>
2024-02-21 15:48:14 +0000

    Taking out logs and changes seeds

--- market/__init__.py
+++ market/__init__.py
@@ -6,9 +6,6 @@ from flask_seeder import FlaskSeeder
 from flask_sqlalchemy import SQLAlchemy
 import sqlalchemy as sa
 import os
-import logging
-from logging.handlers import RotatingFileHandler
-from flask.logging import default_handler
 
 BASEDIR = os.path.abspath(os.path.dirname(__file__))
 
@@ -16,7 +13,7 @@ BASEDIR = os.path.abspath(os.path.dirname(__file__))
 if os.getenv('DATABASE_URL'):
     SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL').replace("postgres://", "postgresql://", 1)
 else:
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///market.db'
+    SQLALCHEMY_DATABASE_URI = 'postgresql://market_amazzon_db_user:d1NVn6KWwW6x1CgtV46kHXkzm4GPWuSs@dpg-cnatn4gl5elc7398foi0-a.frankfurt-postgres.render.com/market_amazzon_db'
 
 
 
@@ -37,7 +34,6 @@ migrate = Migrate(app, db)
 engine = sa.create_engine(app.config['SQLALCHEMY_DATABASE_URI'], pool_pre_ping=True)
 inspector = sa.inspect(engine)
 if not inspector.has_table("user"):
-    # attention
     with app.app_context():
         db.drop_all()
         db.create_all()
@@ -58,34 +54,6 @@ login_manager.login_message_category = "info"
 # Adding record to database
 seeder = FlaskSeeder()
 seeder.init_app(app, db)
-
-
-LOG_WITH_GUNICORN = os.getenv('LOG_WITH_GUNICORN', default=False)
-
-
-def configure_logging(app):
-    # Logging Configuration
-    if LOG_WITH_GUNICORN:
-        gunicorn_error_logger = logging.getLogger('gunicorn.error')
-        app.logger.handlers.extend(gunicorn_error_logger.handlers)
-        app.logger.setLevel(logging.DEBUG)
-    else:
-        file_handler = RotatingFileHandler('instance/flask-user-management.log',
-                                           maxBytes=16384,
-                                           backupCount=20)
-        file_formatter = logging.Formatter('%(asctime)s %(levelname)s %(threadName)s-%(thread)d: %(message)s [in %(filename)s:%(lineno)d]')
-        file_handler.setFormatter(file_formatter)
-        file_handler.setLevel(logging.INFO)
-        app.logger.addHandler(file_handler)
-
-    # Remove the default logger configured by Flask
-    app.logger.removeHandler(default_handler)
-
-    app.logger.info('Starting the Flask User Management App...')
-
-
-configure_logging(app)
-
 
 
 import market.routes
