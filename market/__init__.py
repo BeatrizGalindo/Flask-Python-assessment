8dabea1393994f8ce0ed67d4e407549f879bd428
Beatriz Galindo <beagalin@amazon.com>
2024-02-21 11:13:08 +0000

    Change to add the market db for the new render app

--- market/__init__.py
+++ market/__init__.py
@@ -4,13 +4,46 @@ from flask_login import LoginManager
 from flask_migrate import Migrate
 from flask_seeder import FlaskSeeder
 from flask_sqlalchemy import SQLAlchemy
+import sqlalchemy as sa
+import os
+
+BASEDIR = os.path.abspath(os.path.dirname(__file__))
+
+
+if os.getenv('DATABASE_URL'):
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL').replace("postgres://", "postgresql://", 1)
+else:
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///market.db'
+
+
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://market_db_omwf_user:5lpqByKe2UKV4JZ60QVMFLtl1VKoUI4u@dpg-cnac2ola73kc73emghr0-a/market_db_omwf'
+
+
+
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///market.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = SQLALCHEMY_DATABASE_URI
+
 app.config['SECRET_KEY'] = '1234'
 # Initialise the database
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+
+
+
+# Check if the database needs to be initialized
+engine = sa.create_engine(app.config['SQLALCHEMY_DATABASE_URI'])
+inspector = sa.inspect(engine)
+if not inspector.has_table("users"):
+    # attention
+    with app.app_context():
+        db.drop_all()
+        db.create_all()
+        app.logger.info('Initialized the database!')
+else:
+    app.logger.info('Database already contains the users table.')
+
+
 
 # For the passwords not to be stored in the db as password but as hash
 bcrypt = Bcrypt(app)
@@ -23,6 +56,12 @@ login_manager.login_message_category = "info"
 # Adding record to database
 seeder = FlaskSeeder()
 seeder.init_app(app, db)
+
+
+
+
+
+
 
 
 import market.routes
