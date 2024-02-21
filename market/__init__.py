12362fe71b08beedb94d8a3111b12f9d7e813e5f
Beatriz Galindo <beagalin@amazon.com>
2024-02-21 11:25:40 +0000

    DB is too slow in the cloud adding cofig to fix it

--- market/__init__.py
+++ market/__init__.py
@@ -32,7 +32,7 @@ migrate = Migrate(app, db)
 
 
 # Check if the database needs to be initialized
-engine = sa.create_engine(app.config['SQLALCHEMY_DATABASE_URI'])
+engine = sa.create_engine(app.config['SQLALCHEMY_DATABASE_URI'], pool_pre_ping=True)
 inspector = sa.inspect(engine)
 if not inspector.has_table("users"):
     # attention
