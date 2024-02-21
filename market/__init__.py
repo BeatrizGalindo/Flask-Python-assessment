78c18d17e32549e6081d7229b85aec8c73b23e73
Beatriz Galindo <beagalin@amazon.com>
2024-02-21 11:59:08 +0000

    Adding my logs to render

--- market/__init__.py
+++ market/__init__.py
@@ -6,6 +6,9 @@ from flask_seeder import FlaskSeeder
 from flask_sqlalchemy import SQLAlchemy
 import sqlalchemy as sa
 import os
+import logging
+from logging.handlers import RotatingFileHandler
+from flask.logging import default_handler
 
 BASEDIR = os.path.abspath(os.path.dirname(__file__))
 
@@ -18,7 +21,6 @@ else:
 
 
 app = Flask(__name__)
-
 
 
 # app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///market.db'
@@ -58,9 +60,31 @@ seeder = FlaskSeeder()
 seeder.init_app(app, db)
 
 
+LOG_WITH_GUNICORN = os.getenv('LOG_WITH_GUNICORN', default=False)
 
 
+def configure_logging(app):
+    # Logging Configuration
+    if LOG_WITH_GUNICORN:
+        gunicorn_error_logger = logging.getLogger('gunicorn.error')
+        app.logger.handlers.extend(gunicorn_error_logger.handlers)
+        app.logger.setLevel(logging.DEBUG)
+    else:
+        file_handler = RotatingFileHandler('instance/flask-user-management.log',
+                                           maxBytes=16384,
+                                           backupCount=20)
+        file_formatter = logging.Formatter('%(asctime)s %(levelname)s %(threadName)s-%(thread)d: %(message)s [in %(filename)s:%(lineno)d]')
+        file_handler.setFormatter(file_formatter)
+        file_handler.setLevel(logging.INFO)
+        app.logger.addHandler(file_handler)
 
+    # Remove the default logger configured by Flask
+    app.logger.removeHandler(default_handler)
+
+    app.logger.info('Starting the Flask User Management App...')
+
+
+configure_logging(app)
 
 
 
